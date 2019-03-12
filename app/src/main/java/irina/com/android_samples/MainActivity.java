2657889dec0825cac38b282f3a1f447194440997
Irina <irina.judina@tele2.com>
2019-03-12 17:21:47 +0200

    changes after 1st lecture

--- app/src/main/java/irina/com/android_samples/MainActivity.java
+++ app/src/main/java/irina/com/android_samples/MainActivity.java
@@ -1,7 +1,11 @@
 package irina.com.android_samples;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        RelativeLayout layout = findViewById(R.id.main_layout);
+        if (layout == null) {
+            return;
+        }
+        layout.setBackgroundColor(Color.CYAN);
+    }
+
+    public void onButtonClick(View view) {
+        TextView textViewName = findViewById(R.id.main_text_view);
+        if (textViewName == null) {
+            return;
+        }
+        textViewName.setText("ASDF");
+
+        RelativeLayout layout = findViewById(R.id.main_layout);
+        if (layout == null) {
+            return;
+        }
+        layout.setBackgroundColor(Color.CYAN);
+    }
+
 }
